2cc045de410222a2ccf387030ef6a20eebaf87a4
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2023-06-14 23:43:54 +0500

    Update Seminar_3.swift

--- Seminar_3.swift
+++ Seminar_3.swift
@@ -1,1 +1,17 @@
+print("Task_1 Seminar_3")
 
+var Tea: [String: Int] = ["BlackTea": 100, "GreenTea": 80, "WhiteTea": 150, "RedTea": 130]
+var Queue = ["RedTea", "BlackTea", "BlackTea", "RedTea", "WhiteTea", "GreenTea"]
+print("Общее количество заказов: ")
+print(Queue.count)
+print("Очередность заказов: ")
+for (index, element) in Queue.enumerated() {
+            print(element + " " + String(index + 1) + "-й.")
+}
+print("Стоимость чая: ")
+for(key, value) in Tea {
+    print(key + " " + String(value) + " руб.")
+//    print(value)
+}
+
+print("Task_2 Seminar_3")
