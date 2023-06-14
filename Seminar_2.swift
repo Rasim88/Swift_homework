f2519296e571fd4a8276ddb3f6c930886ab7f09a
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2023-06-14 15:56:57 +0500

    Update Seminar_2.swift

--- Seminar_2.swift
+++ Seminar_2.swift
@@ -1,11 +1,30 @@
+import Foundation
+
 print("Task_1 Seminar_2")
 
+let depositAmount = 10000
+print("Сумма вклада: \(depositAmount)")
 
+let annualRate = 5
+print("Годовая ставка по депозиту: \(annualRate)")
+
+let placementPeriod = 365
+print("Срок размещения денежных средств (в днях): \(placementPeriod)")
+
+let days = 365
+print("Число дней в году: \(days)")
+
+func accruedInterest(depositAmount: Int, annualRate: Int, placementPeriod: Int, days: Int) -> Int {
+    return (depositAmount * annualRate * placementPeriod / days) / 100
+}
+
+let interest = accruedInterest(depositAmount: depositAmount, annualRate: annualRate, placementPeriod: placementPeriod, days: days)
+print("Накопленные проценты: \(interest)")
 
 print("Task_2 Seminar_2")
 
 enum Pizza: CaseIterable {
-  case margherta, diabola, hawaii
+  case margherita, diabola, hawaii
 }
 
 for currentPizza in Pizza.allCases {
