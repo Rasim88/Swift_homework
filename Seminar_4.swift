6a6b6bb06b0a3b6fcf455ee89c82cb86e0adaae1
Rasim Fakhretdinov <111516877+Rasim88@users.noreply.github.com>
2023-06-22 00:45:34 +0500

    Update Seminar_4.swift

--- Seminar_4.swift
+++ Seminar_4.swift
@@ -1,11 +1,24 @@
 print("Task_1 - 5 Seminar_4")
 
-enum PizzaType: String {
-    case margherita
-    case pepperoni
-    case hawaiian
-    case meatLover
-    case veggie
+struct Pizza {
+    let price: Double
+    let crust: CrustType
+    let toppings: [Toppings]
+    
+    enum CrustType: String {
+        case thin
+        case thick
+    }
+    
+    enum PizzaType: String {
+         case margherita
+         case pepperoni
+         case hawaiian
+         case meatLover
+         case veggie
+     }
+     
+     let type: PizzaType
 }
 
 enum Toppings: String {
@@ -15,18 +28,6 @@ enum Toppings: String {
     case onions
     case olives
     case peppers
-}
-
-struct Pizza {
-    let price: Double
-    let type: PizzaType
-    let crust: CrustType
-    let toppings: [Toppings]
-    
-    enum CrustType: String {
-        case thin
-        case thick
-    }
 }
 
 class PizzaShop {
@@ -45,22 +46,21 @@ class PizzaShop {
     }
 }
 
-let margherita = Pizza(price: 8.99, type: .margherita, crust: .thin, toppings: [.extraCheese])
-let pepperoni = Pizza(price: 10.99, type: .pepperoni, crust: .thick, toppings: [.extraCheese, .mushrooms])
-let hawaiian = Pizza(price: 12.99, type: .hawaiian, crust: .thin, toppings: [.onions, .olives])
-let meatLover = Pizza(price: 14.99, type: .meatLover, crust: .thick, toppings: [.mushrooms, .olives, .peppers])
-let veggie = Pizza(price: 11.99, type: .veggie, crust: .thin, toppings: [.peppers, .mushrooms])
+let margherita = Pizza(price: 8.99, crust: .thin, toppings: [.extraCheese], type: .margherita)
+let pepperoni = Pizza(price: 10.99, crust: .thick, toppings: [.extraCheese, .mushrooms], type: .pepperoni)
+let hawaiian = Pizza(price: 12.99, crust: .thin, toppings: [.onions, .olives], type: .hawaiian)
+let meatLover = Pizza(price: 14.99, crust: .thick, toppings: [.mushrooms, .olives, .peppers], type: .meatLover)
+let veggie = Pizza(price: 11.99, crust: .thin, toppings: [.peppers, .mushrooms], type: .veggie)
 
 let pizzaShop = PizzaShop(pizzas: [margherita, pepperoni, hawaiian, meatLover, veggie])
-pizzaShop.addPizza(pizza: Pizza(price: 9.99, type: .meatLover, crust: .thin, toppings: [.onions]))
-pizzaShop.addPizza(pizza: Pizza(price: 13.99, type: .pepperoni, crust: .thick, toppings: [.peppers, .olives]))
+pizzaShop.addPizza(pizza: Pizza(price: 9.99, crust: .thin, toppings: [.onions], type: .meatLover))
+pizzaShop.addPizza(pizza: Pizza(price: 13.99, crust: .thick, toppings: [.peppers, .olives], type: .pepperoni))
 
 for pizza in pizzaShop.getAllPizzas() {
     print("Type: \(pizza.type.rawValue), Price: $\(pizza.price), Crust Type: \(pizza.crust.rawValue), Toppings: \(pizza.toppings.map { $0.rawValue })")
 }
-
 // Вывод:
-// Task_1 Seminar_4
+// Task_1 - 5 Seminar_4
 // Type: margherita, Price: $8.99, Crust Type: thin, Toppings: ["extraCheese"]
 // Type: pepperoni, Price: $10.99, Crust Type: thick, Toppings: ["extraCheese", "mushrooms"]
 // Type: hawaiian, Price: $12.99, Crust Type: thin, Toppings: ["onions", "olives"]
